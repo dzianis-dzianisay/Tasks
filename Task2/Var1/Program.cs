60adde8bd1ca08e82612fa94f6d9b141a0e105a0
dzianis-dzianisay <dzianis.dzianisay@gmail.com>
2021-12-25 18:12:42 +0300

    Task2 two variants implemented

---
+++ Task2/Var1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Shared;
+
+namespace Var1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double k, z, x;              
+            while (true)
+            {
+                bool readResult = ConsoleReader.TryReadDoubleFromConsole("k", out k);
+
+                if (readResult == true)
+                {
+                    break;
+                }
+            }
+            while (true)
+            {
+                bool readResult = ConsoleReader.TryReadDoubleFromConsole("z", out z);
+
+                if (readResult == true)
+                {
+                    break;
+                }
+            }            
+            if(k < 1)
+            {
+                Console.WriteLine($"Вычисление ведем по ветви 1");
+                x = k * Math.Pow(z, 3);
+                var y = Math.Pow(Math.Log(1 + Math.Pow(x, 2)) + Math.Cos(x + 1), Math.Pow(Math.E, k * x));
+                Console.WriteLine($"Значение y = {y} ");                
+            }
+            else if (k >= 1)
+            {
+                Console.WriteLine($"Вычисление ведем по ветви 2");
+                x = z * (z + 1);
+                var y = Math.Pow(Math.Log(1 + Math.Pow(x, 2)) + Math.Cos(x + 1), Math.Pow(Math.E, k * x));
+                if(double.IsInfinity(y))
+                {
+                    Console.WriteLine($"у = бесконечность");
+                }
+                else
+                {
+                    Console.WriteLine($"Значение y = {y} ");
+                }                
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task2/Var2/Program.cs
@@ -0,0 +1,81 @@
+﻿using Shared;
+using System;
+
+namespace Var2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, z, x;
+            while (true)
+            {
+                bool readResult = ConsoleReader.TryReadDoubleFromConsole("a", out a);
+
+                if (readResult == true)
+                {
+                    break;
+                }
+            }
+            while (true)
+            {
+                bool readResult = ConsoleReader.TryReadDoubleFromConsole("b", out b);
+
+                if (readResult == true)
+                {
+                    break;
+                }
+            }
+            while (true)
+            {
+                bool readResult = ConsoleReader.TryReadDoubleFromConsole("z", out z);
+
+                if (readResult == true)
+                {
+                    break;
+                }
+            }
+            if (z < a * b)
+            {
+                Console.WriteLine($"Вычисление ведем по ветви 1");
+                x = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2) * z);
+                if (x < 0)
+                {
+                    Console.WriteLine($"Невозможно извлечь корень из отрицательного числа");
+                }
+                else
+                {
+                    var numerator = a * x + b * Math.Cos(Math.Sqrt(x));
+                    var denumerator = x + a * b;
+                    if (denumerator == 0)
+                    {
+                        Console.WriteLine($"Делить на ноль нельзя");
+                    }
+                    else
+                    {
+                        var y = numerator / denumerator;
+                        Console.WriteLine($"Значение y = {y} ");
+                    }
+                }
+            }
+            else if (z >= a * b)
+            {
+                Console.WriteLine($"Вычисление ведем по ветви 2");
+                x = Math.Pow(Math.Sin(z), 2) + Math.Abs(a * b * z);
+                var numerator = a * x + b * Math.Cos(Math.Sqrt(x));
+                var denumerator = x + a * b;
+                if (denumerator == 0)
+                {
+                    Console.WriteLine($"Делить на ноль нельзя");
+                }
+                else
+                {
+                    var y = numerator / denumerator;
+                    Console.WriteLine($"Значение y = {y} ");
+                }
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
