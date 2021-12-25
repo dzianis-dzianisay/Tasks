ae3b6c7b3dcada50090350ac0dd3de84020217b0
dzianis-dzianisay <dzianis.dzianisay@gmail.com>
2021-12-25 18:33:59 +0300

    dirty commit

---
+++ Task3/Var2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Var2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
